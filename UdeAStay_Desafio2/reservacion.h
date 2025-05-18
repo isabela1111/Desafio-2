06d6567e1b942f254ab0894e0bc654025af72a01
isabela1111 <isabela.aguirre@udea.edu.co>
2025-05-18 17:56:05 -0500

    se añade cabecera de la clase reservacion

--- UdeAStay_Desafio2/reservacion.h
+++ UdeAStay_Desafio2/reservacion.h
@@ -1,10 +1,37 @@
 #ifndef RESERVACION_H
 #define RESERVACION_H
 
-class Reservacion
-{
+#include "fecha.h"
+
+class Reservacion {
+private:
+    char codigo[15];
+    char documentoHuesped[20];
+    char codigoAlojamiento[10];
+    Fecha fechaInicio;
+    int duracionNoches;
+    char metodoPago[10];
+    float monto;
+    char anotaciones[1001];
+
 public:
     Reservacion();
+    Reservacion(const char* codRes, const char* docH, const char* codAloj, Fecha inicio,int noches,
+                const char* metodo, float monto, const char* nota);
+
+    // Getters
+    const char* getCodigo() const;
+    const char* getDocumentoHuesped() const;
+    const char* getCodigoAlojamiento() const;
+    Fecha getFechaInicio() const;
+    int getDuracion() const;
+    const char* getMetodoPago() const;
+    float getMonto() const;
+    const char* getAnotaciones() const;
+
+    void mostrar() const;
+    void activa();
+    void finalizada();
 };
 
-#endif // RESERVACION_H
+#endif
