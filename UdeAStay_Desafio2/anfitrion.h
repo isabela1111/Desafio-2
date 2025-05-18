819accdd715d6ce1e5ca8310e6dcf6beb939edae
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2025-05-18 09:14:58 -0500

    Se agregan cabeceras de la clase Anfitrion

--- UdeAStay_Desafio2/anfitrion.h
+++ UdeAStay_Desafio2/anfitrion.h
@@ -1,10 +1,25 @@
 #ifndef ANFITRION_H
 #define ANFITRION_H
 
-class Anfitrion
-{
+class Anfitrion {
+private:
+    char* documento;
+    int antiguedad;
+    int puntuacion;
+    int* codigosAlojamientos;
+    int numAlojamientos;
+
 public:
     Anfitrion();
+    Anfitrion(const char* doc, int ant, int punt, int* cods, int numCods);
+    ~Anfitrion();
+
+    //Funcion de prueba
+    //void mostrarInfo() const;
+
+    //Funciones en desarrollo
+    void verReservas();         
+    void actualizarHistorico();
 };
 
 #endif // ANFITRION_H
