8c3bcf3529acfc8dea4b360e5dad9eefc27e0231
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2025-05-18 14:39:54 -0500

    Se agrega los cabeceras en fecha.h

--- UdeAStay_Desafio2/fecha.h
+++ UdeAStay_Desafio2/fecha.h
@@ -1,10 +1,32 @@
 #ifndef FECHA_H
 #define FECHA_H
 
-class Fecha
-{
+class Fecha {
+private:
+    int dia;
+    int mes;
+    int anio;
+
 public:
     Fecha();
+    Fecha(int d, int m, int a);
+
+    int getDia() const;
+    int getMes() const;
+    int getAnio() const;
+    void setDia(int d);
+    void setMes(int m);
+    void setAnio(int a);
+
+    // Funciones de utilidad
+    bool esValida() const;
+    bool estaEnRango12Meses(const Fecha& actual) const;
+    bool esMenorQue(const Fecha& otra) const;
+    bool esIgualA(const Fecha& otra) const;
+    void mostrar() const;
+
+    
+    Fecha& operator=(const Fecha& otra);
 };
 
-#endif // FECHA_H
+#endif 
