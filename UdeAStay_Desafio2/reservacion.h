d31ba2eae51b03344a328c76cfcd4b1a9ef030e8
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2025-05-19 13:06:00 -0500

    modificacion de la cabecera reservacion para incluir pago

--- UdeAStay_Desafio2/reservacion.h
+++ UdeAStay_Desafio2/reservacion.h
@@ -2,6 +2,7 @@
 #define RESERVACION_H
 
 #include "fecha.h"
+#include "pago.h"
 
 class Reservacion {
 private:
@@ -10,29 +11,24 @@ private:
     char codigoAlojamiento[10];
     Fecha fechaInicio;
     int duracionNoches;
-    char metodoPago[10];
-    float monto;
+    Pago pago; 
     char anotaciones[1001];
 
 public:
     Reservacion();
-    Reservacion(const char* codRes, const char* docH, const char* codAloj, Fecha inicio,int noches,
-                const char* metodo, float monto, const char* nota);
+    Reservacion(const char* codRes, const char* docH, const char* codAloj,
+                Fecha inicio, int noches, const Pago& pago, const char* nota);
 
-    // Getters
     const char* getCodigo() const;
     const char* getDocumentoHuesped() const;
     const char* getCodigoAlojamiento() const;
     Fecha getFechaInicio() const;
     int getDuracion() const;
-    const char* getMetodoPago() const;
-    float getMonto() const;
+    Pago getPago() const;
     const char* getAnotaciones() const;
 
     void mostrar() const;
     bool activa(const Fecha& fechaCorte) const;
-    void finalizada();
 };
 
 #endif
-
