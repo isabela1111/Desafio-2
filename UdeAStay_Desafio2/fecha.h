a87368cf45c96c63949c2b3f51b03c0e885cbd2d
isabela1111 <isabela.aguirre@udea.edu.co>
2025-05-19 18:29:58 -0500

    Correción en Clase fecha

--- UdeAStay_Desafio2/fecha.h
+++ UdeAStay_Desafio2/fecha.h
@@ -1,5 +1,6 @@
 #ifndef FECHA_H
 #define FECHA_H
+
 class Fecha {
 private:
     int dia;
@@ -17,18 +18,15 @@ public:
     void setMes(int m);
     void setAnio(int a);
 
-  
     bool esMenorQue(const Fecha& otra) const;
     bool esIgualA(const Fecha& otra) const;
-    void mostrar() const
+    void mostrar() const;   // <- Aquí faltaba el punto y coma
     Fecha& operator=(const Fecha& otra);
     bool validarFecha() const;
     void imprimirFechaLarga() const;
+
     Fecha sumarDias(int dias) const;
     int comparar(const Fecha& otra) const;
-
-
-
 };
 
-#endif 
+#endif
